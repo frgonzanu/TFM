91f830c6a1d86afb29568c0dd88def5cc0c0016b
frgonzanu <pacogonzalez.1995@gmail.com>
2026-02-27 11:27:29 +0100

    update

--- Scripts/Data_Download.R
+++ Scripts/Data_Download.R
@@ -1,22 +1,33 @@
-library(future.apply)
-plan(multisession, workers = 6)
+#Download of GZs
 
 safe_download <- function(url, destfile) {
   if (file.exists(destfile)) {
-    message("Ya existe: ", destfile, " — se omite.")
     return(destfile)
   }
   download.file(url, destfile, mode = "wb")
   return(destfile)
 }
 
-future_mapply(
-  FUN = safe_download,
-  url = sub_data$supplementary_file_1,
-  destfile = file.path("GSE97693/TPMs", files$name)
-)
+library(GEOquery)
+library(dplyr)
+
+sub_data <- pData(getGEO("GSE97693")$GSE97693_series_matrix.txt.gz) %>%
+  filter(stringr::str_detect(supplementary_file_1, "TPM"))
+
+all(list.files("GSE97693/TPMs/") == basename(sub_data$supplementary_file_1))
+
+if(all(list.files("GSE97693/TPMs/") == basename(sub_data$supplementary_file_1))){
+  print("Todos los archivos han sido descargados.")
+}else{
+  mapply(
+    FUN = safe_download,
+    url = sub_data$supplementary_file_1,
+    destfile = file.path("GSE97693/TPMs", basename(sub_data$supplementary_file_1))
+  )
+}
 
 
+# Extraction of TXTs from .GZs
 
 library(R.utils)
 
